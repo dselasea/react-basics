1ea7773d47db3f827e56723cc952caa94befe5bf
Daniel Selase Anyidoho <dselasea@gmail.com>
2021-11-01 17:28:10 +0000

    Learned props and implemented props.children

--- src/index.js
+++ src/index.js
@@ -3,22 +3,40 @@ import ReactDom  from "react-dom";
 // adding css
 import "./index.css";
 
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/910N36lR%2BkL._AC_UL200_SR200,200_.jpg",
+  title: "Dune(Dune Chronicles, Book 1)",
+  author: "Frank Herbert"
+
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/91DEviT3oUL._AC_UL200_SR200,200_.jpg",
+  title: "How to Catch a Turkey",
+  author: "Adam Wallace"
+
+};
+
 function BookList(){
   return (
     <section className="booklist">
-      <Book />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique blanditiis dolor mollitia culpa obcaecati doloribus, corrupti ipsa nam, rerum eum incidunt ea quibusdam sunt, voluptatibus corporis iusto? Ratione, distinctio in!</p>
+      </Book>
     </section>
   );
 }
 
-const author = "Frank Herbert";
-const Book = () => {
-  const title = "Dune(Dune Chronicles, Book 1)";
+// The book component is a function
+const Book = (props) => {
+  const {img, title, author} = props;
   return ( 
     <article className="book">
-     <img src="https://images-na.ssl-images-amazon.com/images/I/910N36lR%2BkL._AC_UL200_SR200,200_.jpg" alt="" />
+     <img src={img} alt="" />
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author}</h4>
+      {props.children}
     </article>
    );
 }
