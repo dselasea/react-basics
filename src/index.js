5940f5bbef0cbd52c01f7cd34b5e864f15518a45
Daniel Selase Anyidoho <dselasea@gmail.com>
2021-11-01 19:46:15 +0000

    Learned more about props and rendering lists with map method

--- src/index.js
+++ src/index.js
@@ -3,40 +3,55 @@ import ReactDom  from "react-dom";
 // adding css
 import "./index.css";
 
-const firstBook = {
+const books = [{
   img: "https://images-na.ssl-images-amazon.com/images/I/910N36lR%2BkL._AC_UL200_SR200,200_.jpg",
   title: "Dune(Dune Chronicles, Book 1)",
   author: "Frank Herbert"
 
-};
+},
 
-const secondBook = {
+{
   img: "https://images-na.ssl-images-amazon.com/images/I/91DEviT3oUL._AC_UL200_SR200,200_.jpg",
   title: "How to Catch a Turkey",
   author: "Adam Wallace"
 
-};
+},
+{
+  img: "https://images-na.ssl-images-amazon.com/images/I/81YHHiNPDNL._AC_UL200_SR200,200_.jpg",
+  title: "The Storyteller: Tales of Life and Music",
+  author: "Dave Grohl"
+
+},
+
+{
+  img: "https://images-na.ssl-images-amazon.com/images/I/71xPLm2FenL._AC_UL200_SR200,200_.jpg",
+  title: "Taste: My Life Through Food",
+  author: "Stanley Tucci"
+}];
+
 
 function BookList(){
   return (
     <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique blanditiis dolor mollitia culpa obcaecati doloribus, corrupti ipsa nam, rerum eum incidunt ea quibusdam sunt, voluptatibus corporis iusto? Ratione, distinctio in!</p>
-      </Book>
+      {books.map((book) => {
+        const {img, title, author} = book;
+        return (
+          <Book book={book} />
+        )
+      })}
     </section>
   );
 }
 
 // The book component is a function
+
 const Book = (props) => {
-  const {img, title, author} = props;
+  const {img, title, author} = props.book;
   return ( 
     <article className="book">
      <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
    );
 }
