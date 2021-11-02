212d574b0d313a79e88fb794d50f1f566b128868
Daniel Selase Anyidoho <dselasea@gmail.com>
2021-11-02 18:05:05 +0000

    Working with event in react

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDom  from "react-dom";
 import "./index.css";
 
 const books = [{
+  id: 1,
   img: "https://images-na.ssl-images-amazon.com/images/I/910N36lR%2BkL._AC_UL200_SR200,200_.jpg",
   title: "Dune(Dune Chronicles, Book 1)",
   author: "Frank Herbert"
@@ -11,12 +12,14 @@ const books = [{
 },
 
 {
+  id: 2,
   img: "https://images-na.ssl-images-amazon.com/images/I/91DEviT3oUL._AC_UL200_SR200,200_.jpg",
   title: "How to Catch a Turkey",
   author: "Adam Wallace"
 
 },
 {
+  id: 3,
   img: "https://images-na.ssl-images-amazon.com/images/I/81YHHiNPDNL._AC_UL200_SR200,200_.jpg",
   title: "The Storyteller: Tales of Life and Music",
   author: "Dave Grohl"
@@ -24,6 +27,7 @@ const books = [{
 },
 
 {
+  id: 4,
   img: "https://images-na.ssl-images-amazon.com/images/I/71xPLm2FenL._AC_UL200_SR200,200_.jpg",
   title: "Taste: My Life Through Food",
   author: "Stanley Tucci"
@@ -34,9 +38,8 @@ function BookList(){
   return (
     <section className="booklist">
       {books.map((book) => {
-        const {img, title, author} = book;
         return (
-          <Book book={book} />
+          <Book key={book.id} {...book} />
         )
       })}
     </section>
@@ -46,12 +49,18 @@ function BookList(){
 // The book component is a function
 
 const Book = (props) => {
-  const {img, title, author} = props.book;
+  const {img, title, author} = props;
+  const complexExample = (author) => {
+      console.log(author);
+  }
+
   return ( 
     <article className="book">
      <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onMouseOver={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button">complex example</button>
+      <button type="button" onClick={complexExample(author)}>more complex example</button>
     </article>
    );
 }
